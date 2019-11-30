feeaecf210a5ce648d49640b9e52643a31a4a59b
Christopher Doris <github.com/cjdoris>
2019-11-30 20:20:39 +0000

    Upgrade to using `Pkg.Artifacts`.

--- deps/build.jl
+++ deps/build.jl
@@ -1,13 +1,18 @@
-const table_url = "https://www.issn.org/wp-content/uploads/2013/09/LTWA_20160915.txt"
-const table_file = joinpath(@__DIR__, "LTWA_20160915.txt")
+using Pkg.Artifacts
 
-if !isfile(table_file)
-	@info "Downloading $(repr(table_url)) to $(repr(table_file))"
-	download(table_url, table_file)
-end
+const url = "https://www.issn.org/wp-content/uploads/2013/09/LTWA_20160915.txt"
+const toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+const file = "LTWA.txt"
+const key  = "LTWA"
 
-@assert isfile(table_file)
+h = artifact_hash(key, toml)
+
+artifact_exists(h) ||
+	create_artifact(dir -> download(url, joinpath(dir, file))) == h ||
+		error("LTWA file has changed")
+
+const path = joinpath(artifact_path(h), file)
 
 open("deps.jl", "w") do io
-	println(io, "const ltwa_file = $(repr(table_file))")
+	println(io, :(const ltwa_file = $path))
 end
